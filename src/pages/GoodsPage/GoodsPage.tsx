953f9b4cb459f17da313ace92d2edf076cfb80cb
wuchuheng <root@wuchuheng.com>
2022-11-18 19:12:35 +0800

    feat(ui): init order and goods page.

---
+++ src/pages/GoodsPage/GoodsPage.tsx
@@ -0,0 +1,58 @@
+/**
+ *  This file is part of emailNotes.
+ *
+ * @Description Say something for this file.
+ * @Author      wuchuheng<root@wuchuheng.com>
+ * @Time        2022/11/18 19:09
+ */
+import React from "react";
+import { View } from "@tarojs/components";
+
+const GoodsPage: React.FC = () => {
+  return (
+    <>
+      <View>Goods</View>
+      <View>1</View>
+      <View>2</View>
+      <View>3</View>
+      <View>4</View>
+      <View>5</View>
+      <View>6</View>
+      <View>7</View>
+      <View>8</View>
+      <View>9</View>
+      <View>10</View>
+      <View>11</View>
+      <View>12</View>
+      <View>13</View>
+      <View>14</View>
+      <View>15</View>
+      <View>16</View>
+      <View>17</View>
+      <View>18</View>
+      <View>19</View>
+      <View>1</View>
+      <View>1</View>
+      <View>2</View>
+      <View>3</View>
+      <View>4</View>
+      <View>5</View>
+      <View>6</View>
+      <View>7</View>
+      <View>8</View>
+      <View>9</View>
+      <View>10</View>
+      <View>11</View>
+      <View>12</View>
+      <View>13</View>
+      <View>14</View>
+      <View>15</View>
+      <View>16</View>
+      <View>17</View>
+      <View>18</View>
+      <View>19</View>
+    </>
+  );
+};
+
+export default GoodsPage;
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -6,13 +6,15 @@ import "./index.less";
 import MainLayout, { TabItemType } from "../../layout/mainLayout/MainLauout";
 import { IconName } from "../../components/Icons/Icons";
 import HomePage from "../HomePage/HomePage";
+import OrderPage from "../OrderPage/OrderPage";
+import GoodsPage from "../GoodsPage/GoodsPage";
 
 const Index: React.FC = () => {
-  const [activeIndex, setActiveIndex] = useState<number>(0);
+  const [activeIndex, setActiveIndex] = useState<number>(1);
   const tabs: TabItemType[] = [
     { content: <HomePage />, name: "主页", icon: IconName.HOME },
-    { content: <View>orders</View>, name: "清单", icon: IconName.ORDER },
-    { content: <View>menu</View>, name: "汇总", icon: IconName.MENU },
+    { content: <GoodsPage />, name: "货物", icon: IconName.MENU },
+    { content: <OrderPage />, name: "账单", icon: IconName.ORDER },
   ];
   const onChange = (newIndex: number) => {
     setActiveIndex(newIndex);
